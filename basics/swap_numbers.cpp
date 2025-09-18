aa4a11faae99ced46ce4b0cfc566d6cd4cbf1020
aarayamansrivastava1110-create <aarayaman.srivastava1110@gmail.com>
2025-09-18 12:03:20 +0530

    Create swap_numbers.cpp

---
+++ basics/swap_numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//SWAPPING 2 NUMBERS
+
+int main(){
+  int x = 5;
+  int y = 6;
+  int z;
+  
+  z = x;
+  x = y;
+  y = z;
+
+  cout<<"AFTER SWAPPING";
+  cout<< x << endl;
+  cout<< y << endl;
+  
+  return 0;
+}
