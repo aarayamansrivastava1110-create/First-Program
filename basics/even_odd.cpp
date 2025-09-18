85301b502312a6432dc4739cd92f9ac37dd3c8b6
aarayamansrivastava1110-create <aarayaman.srivastava1110@gmail.com>
2025-09-18 12:17:50 +0530

    Create even_odd.cpp

---
+++ basics/even_odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x ;
+    cout<<"PLEASE ENTER THE NUMBER YOU WANT TO CHECK"<< endl;
+    cin>> x;
+  
+    if ( (x % 2)==0){
+        cout<<"EVEN";
+    }
+      
+    else {
+        cout<<"ODD";
+    }
+
+}
