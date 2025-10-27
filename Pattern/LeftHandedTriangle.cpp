5d7e1f8bd83a408c0c6c87503f4a949de712044c
aarayamansrivastava1110-create <aarayaman.srivastava1110@gmail.com>
2025-10-27 12:12:47 +0530

    Fix include directive and improve triangle pattern

---
+++ Pattern/LeftHandedTriangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,i,j,k;
+    cout<<"PLS ENTER NUMBER OF ROWS:"<<endl;
+    cin>>n;
+
+    for (i=1;i<=n;i++){
+        for(j=n-i; j>0;j--){
+            cout<<" ";
+        }
+        for(k=1;k<=i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
